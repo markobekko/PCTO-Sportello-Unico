e01199dda0479442a12b5f234d1cca2aa5752914
titti <90624553+titti154@users.noreply.github.com>
2022-02-14 09:27:31 +0100

    Add files via upload

--- iscrizione.php
+++ iscrizione.php
@@ -29,29 +29,6 @@
                             <input type="text" required> 
                     </div>
                 </div>
-
-                <span class="detail">Scelta sede e data:</span> <!-- nome davanti alla label -->
-
-                <div class="ciao">
-                    <?php
-                        $conn = new mysqli('localhost', 'root', '', 'pcto') 
-                        or die('Cannot connect to db');
-                        
-                        $result = $conn -> query("select ID, data_esame, sede_esame from sessione_esame");
-                        echo "<select class='provami' data_esame = 'id'>";
-                            while ($row = $result->fetch_assoc()){
-                                unset($id, $data_esame, $sede_esame);
-                                $id = $row['id'];
-                                $data_esame = $row['data_esame']; 
-                                $sede_esame = $row['sede_esame'];
-                                echo '<option value= "'.$id.'">'.$sede_esame." - ".$data_esame.'</option>';
-                            }
-                        echo "</select>";
-                    ?>
-                </div>
-                <br>
-                <br>
-                
                 <div class="button">
                     <input type="submit" value="Registra nominativo">
                 </div>
